3c945db0aee9d4e0d346eb2b9d32fe1d78f48729
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-26 15:03:02 +0200

    Task 1 - color switcher
    Performed this task in 01-color-switcher.html and 01-color-switcher.js files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener("click", () => {
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    
+    intervalId = setInterval(() => {  
+
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;      
+
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+
+    stopBtn.disabled = true;
+    startBtn.disabled = false;  
+    
+  clearInterval(intervalId);  
+});

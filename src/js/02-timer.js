a413afc48967fb0f9b76ca6205b2826aa1efaae5
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-26 15:16:15 +0200

    Update 02-timer.js

---
+++ src/js/02-timer.js
@@ -0,0 +1,5 @@
+const startBtn = document.querySelector('button[data-start]');
+const daysSpan = document.querySelector('span[data-days]');
+const hoursSpan = document.querySelector('span[data-hours]');
+const minutesSpan = document.querySelector('span[data-minutes]');
+const secondsSpan = document.querySelector('span[data-seconds]');

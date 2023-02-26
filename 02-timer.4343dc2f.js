80bdb2a5a4e019d7d63a296b9b1ef5d81c60fa9a
EuJinnLucaShow <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-02-26 13:16:49 +0000

    Deploying to gh-pages from @ EuJinnLucaShow/goit-js-hw-09@a413afc48967fb0f9b76ca6205b2826aa1efaae5 🚀

---
+++ 02-timer.4343dc2f.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("span[data-days]"),document.querySelector("span[data-hours]"),document.querySelector("span[data-minutes]"),document.querySelector("span[data-seconds]");
+//# sourceMappingURL=02-timer.4343dc2f.js.map

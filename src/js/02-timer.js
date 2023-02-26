2ee1ed25ca87ff6884280f52026fecdf3d865d90
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-26 20:34:11 +0200

    Summary (required)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,47 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+// import Notiflix from 'notiflix';
+// import "dist/notiflix-aio-3.2.6.min.js"
+
+const datetimeInput = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 const daysSpan = document.querySelector('span[data-days]');
 const hoursSpan = document.querySelector('span[data-hours]');
 const minutesSpan = document.querySelector('span[data-minutes]');
 const secondsSpan = document.querySelector('span[data-seconds]');
+
+flatpickr(datetimeInput, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },});
+
+  function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+startBtn.addEventListener("click", () => {    
+  const time = convertMs(); 
+});
